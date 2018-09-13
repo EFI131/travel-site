0d3cb2156e4f7308778b3a5041086a27866c1797
EFI131 <efi131@gmail.com>
2018-09-14 04:36:31 +0800

    commited all sprite manipulations

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,28 +1,11 @@
 var gulp = require('gulp');
-
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var cssvars = require("postcss-simple-vars");
 var nested = require("postcss-nested");
 var cssImport = require("postcss-import");
 var mixins = require('postcss-mixins');
-
-
 const browserSync = require('browser-sync').create();
-
-gulp.task('styles', function () {
-    return gulp
-        .src("./app/assets/styles/styles.css")
-        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
-        .on('error', function (errorInfo) {
-            console.log(errorInfo.toString());
-            this.emit('end');
-        })
-        .pipe(gulp.dest("./app/temp/styles"))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-});
 
 gulp.task('serve', function (done) {
     browserSync.init({
