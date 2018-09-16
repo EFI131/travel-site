ffbf1efd00f6759bb5b82762f76132355ec704bf
EFI131 <efi131@gmail.com>
2018-09-16 23:31:56 +0800

    Completed lazy loading of images

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,9 @@
 import $ from "jquery";
-
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 import Modal from './modules/Modal';
+
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "80%");
